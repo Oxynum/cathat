7be8e673761fd525571579636894acf70093d4f7
Thomas THIMOTHEE <thomas@oxynum.fr>
2014-02-17 05:21:07 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cathat::Application.config.secret_key_base = 'fad1c1183ff81eec7385c1dba1db2143c2cb4eb561c3ac43ee7e9f136536d225be40b25bf533095e29c5391343ebcd9ad744e568282c3c09c1d3af7539166b4e'
