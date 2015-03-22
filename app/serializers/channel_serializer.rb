50ef25d6cf483efe469fe93b77dd3f8586958218
Thomah <thomas@oxynum.fr>
2015-03-22 13:11:59 +0100

    add image to channel json

--- app/serializers/channel_serializer.rb
+++ app/serializers/channel_serializer.rb
@@ -1,5 +1,13 @@
 class ChannelSerializer < ActiveModel::Serializer
-  attributes :id, :title, :latitude, :longitude, :creator_id, :created_at, :updated_at
+  attributes :id, :title, :latitude, :longitude, :creator_id, :created_at, :updated_at, :image_urls
   has_many :messages
   has_many :subscribers
+
+  def image_urls
+    result = {}
+    [nil, :large, :medium, :small, :thumb].each do |version|
+      result[version||:normal] =  object.image.url(version)
+    end
+    result
+  end
 end
